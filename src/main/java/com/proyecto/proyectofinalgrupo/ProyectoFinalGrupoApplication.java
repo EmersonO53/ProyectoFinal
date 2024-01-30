8d42bb99baa75e881d3e03eafc7bee666b5c2ce9
Emerson <emerson.orosco.est@tecazuay.edu.ec>
2024-01-29 22:14:50 -0500

    primercommit

---
+++ src/main/java/com/proyecto/proyectofinalgrupo/ProyectoFinalGrupoApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyectofinalgrupo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalGrupoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalGrupoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/proyectofinalgrupo/ProyectoFinalGrupoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyectofinalgrupo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalGrupoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
